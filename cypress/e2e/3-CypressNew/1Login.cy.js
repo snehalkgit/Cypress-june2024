091c4771d974ffa35208f87797fbd16a22800008
snehal kamble <you@example.com>
2024-06-18 20:01:09 +0530

    added

--- cypress/e2e/3-CypressNew/1Login.cy.js
+++ cypress/e2e/3-CypressNew/1Login.cy.js
@@ -1,30 +1,29 @@
 ///<reference types="cypress" />
 
-describe('Verify contact us form functionality ',function(){
+describe('Verify contact us form functionality ', function () {
 
-    it('verify the contact us form with valid data',()=>{
-       cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
-       cy.get('[name="first_name"]').type("snehal")
-       cy.get('[name="last_name"]').type("kamble")
-       cy.get('[name="email"]').type("snehalk1234@gmail.com")
-       cy.get('[name="message"]').type('im learning automation testing')
-       cy.get('[type="submit"]').click()
-       cy.get('h1').should('have.text','Thank You for your Message!')
-       cy.get('h1').should('contain','Thank You')
+    it('verify the contact us form with valid data', () => {
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.get('[name="first_name"]').type("snehal")
+        cy.get('[name="last_name"]').type("kamble")
+        cy.get('[name="email"]').type("snehalk1234@gmail.com")
+        cy.get('[name="message"]').type('im learning automation testing')
+        cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text', 'Thank You for your Message!')
+        cy.get('h1').should('contain', 'Thank You')
     })
-    it('verify the cintact us form with invalid data',()=>{
+    it('verify the cintact us form with invalid data', () => {
         cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html")
         cy.get('[name="first_name"]').type("snehal")
-       cy.get('[name="last_name"]').type("kamble")
-       cy.get('[name="email"]').type("snehalk1234@.com")
-       cy.get('[name="message"]').type('im learning automation testing')
-       cy.get('[type="submit"]').click()
-       cy.contains('Error: Invalid email address').should('be.visible')
-      
-     
-    
+        cy.get('[name="last_name"]').type("kamble")
+        cy.get('[name="email"]').type("snehalk1234@.com")
+        cy.get('[name="message"]').type('im learning automation testing')
+        cy.get('[type="submit"]').click()
+        cy.contains('Error: Invalid email address').should('be.visible')
+
+
+
     })
 
 
 })
-    
